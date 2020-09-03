746da79697c3ba9a98b4b9a48db711244d2c4219
matija <matija@matija-Aspire-A517-51G>
2020-09-03 14:51:42 +0200

    Added controller function which creates a recipe panel from a recipe

--- src/controller/Kontroler.java
+++ src/controller/Kontroler.java
@@ -5,10 +5,28 @@
  */
 package controller;
 
+import java.time.format.DateTimeFormatter;
+import model.Recipe;
+import view.RecipePanel;
+
 /**
  *
  * @author Jovana
  */
 public class Kontroler {
     
+    public RecipePanel createRecipePanel(Recipe r) {
+        RecipePanel rp = new RecipePanel();
+        rp.getDateLabel().setText(r.getCreationDate()
+                .format(DateTimeFormatter
+                .ofPattern("dd.MM.yyyy. HH:mm")));
+        rp.getNameLabel().setText(r.getName());
+        /* If instructions are longer than 280 chars, it chopps it off */
+        /* <html> tags enable the formatting of text in the label      */
+        rp.getInstructionsLabel().setText("<html>" + r.getText().substring(0, 
+                Math.min(r.getText().length(), 280)) + "...</html>");
+        rp.getReviewsLabel().setText(Double.toString(r.calculateGradeAvg()));
+        rp.getIngredientsLabel().setText(r.getIngredientsString());
+        rp.getAppliancesLabel().setText(r.getAppliancesString());
+    }
 }
