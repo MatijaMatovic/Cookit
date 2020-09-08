aee667ff65e8ff91582e9cdc0e60516c51f77e35
matija <matija@matija-Aspire-A517-51G>
2020-09-08 13:59:44 +0200

    Fixed imports

--- src/controller/ViewController.java
+++ src/controller/ViewController.java
@@ -14,7 +14,6 @@ import java.awt.event.ComponentEvent;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Map;
